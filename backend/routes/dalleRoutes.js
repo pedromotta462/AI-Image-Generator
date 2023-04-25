ce68b837b4afd0fdd7f21994fadbed3d4b8c00af
pedromotta462 <pedromotta1d2015@gmail.com>
2023-04-25 16:11:50 -0300

    some adjusts

--- backend/routes/dalleRoutes.js
+++ backend/routes/dalleRoutes.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import { Configuration, OpenAIApi } from 'openai';
+import { OPENAI_API_KEY } from '../config.js';
 
 dotenv.config();
 
 const router = express.Router();
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
